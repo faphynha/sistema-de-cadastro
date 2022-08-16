b0fa6f1186efa25ce6d04e54c545b35743050aaa
faphynha <110789673+faphynha@users.noreply.github.com>
2022-08-15 21:17:44 -0300

    mudei o console

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
-console.log("Rafaela adicionou mais frases")
+//mais um cosole log de teste:
 console.log("Vamos sempre fazer o bem independente do que nos façam")
 console.log("Mesmo que nos façam o mal, vamos devolver com o bem")
 console.log("O mundo seria bem melhor sem pessoas más")
